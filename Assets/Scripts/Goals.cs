3e39c2c3cbc4d1588fcf33df2df186a9a113b732
Axel Kennedy <n9751866@qut.edu.au>
2020-01-30 15:35:51 +1000

    Scoring System Implemented

--- Assets/Scripts/Goals.cs
+++ Assets/Scripts/Goals.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Goals : MonoBehaviour
 {
     [SerializeField] private Ball ball;
+    [SerializeField] private int playerGoal;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Ball")) ball.Reset();
+        ScoreManager.Instance.UpdatePlayerScore(playerGoal);
     }
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    //singleton variable
+    public static ScoreManager Instance;
+
+    // Score tracking
+    private int playerOneScore, playerTwoScore;
+    [SerializeField] private Text playerOneScoreText, playerTwoScoreText;
+    [SerializeField] int noOfPlayers = 2;
+    const int Player_One_Index = 0;
+
+    void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(this);
+        } else
+        {
+            Instance = this;
+        }
+
+        playerOneScore = 0;
+        playerTwoScore = 0;
+    }
+
+    // Update the player's score
+    public void UpdatePlayerScore(int player)
+    {
+        if (player == Player_One_Index)
+        {
+            playerTwoScore++;
+            playerTwoScoreText.text = playerTwoScore.ToString();
+        } else
+        {
+            playerOneScore++;
+            playerOneScoreText.text = playerOneScore.ToString();
+        }
+    }
+}
